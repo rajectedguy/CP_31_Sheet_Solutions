cefcff43911ba53ffb914f04d04290f5914a0879
rajectedguy <rajectedguy@gmail.com>
2026-07-10 21:47:36 +0530

    Problem 8 - B. Comparison String

--- Cp31Rated900/src/BComparisonString.java
+++ Cp31Rated900/src/BComparisonString.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class BComparisonString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            long n = scanner.nextLong();
+            String s = scanner.next();
+            long longestSubstringLength = 1;
+            long currentSubstringLength = 1;
+            for (int i = 1; i < n; i++) {
+                if (s.charAt(i) == s.charAt(i - 1)) {
+                    currentSubstringLength++;
+                } else {
+                    longestSubstringLength = Math.max(longestSubstringLength, currentSubstringLength);
+                    currentSubstringLength = 1;
+                }
+            }
+            longestSubstringLength = Math.max(longestSubstringLength, currentSubstringLength);
+            System.out.println(longestSubstringLength + 1);
+        }
+        scanner.close();
+    }
 }
