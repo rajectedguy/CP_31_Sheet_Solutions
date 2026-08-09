86b1796a9bd7bf8961433b9dd004616440811c7b
rajectedguy <rajectedguy@gmail.com>
2026-08-09 23:10:04 +0530

    Problem 11 - B. Basketball Together

---
+++ Cp31Rated1000/src/BBeautifulArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BBeautifulArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            long n = scanner.nextLong();
+            long k = scanner.nextLong();
+            long b = scanner.nextLong();
+            long s = scanner.nextLong();
+            long minimum_s = k * b;
+            long maximum_s = (k * b) + (k - 1) * n;
+            if (s < minimum_s || s > maximum_s) {
+                System.out.println("-1");
+            } else {
+                long[] ans = new long[(int) n];
+                ans[0] = minimum_s;
+                s -= minimum_s;
+                for (int i = 0; i < n; i++) {
+                    long add = Math.min(k - 1, s);
+                    ans[i] += add;
+                    s -= add;
+                }
+                for (long value : ans) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+        }
+        scanner.close();
+    }
+}
